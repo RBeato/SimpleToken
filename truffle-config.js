9c3adb9fdfecd88a6e429514bad1d7c22ed17e4a
Romeu Beato <romeufbeato@gmail.com>
2021-05-10 13:34:19 +0100

    other

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,54 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+
 module.exports = {
   networks: {
     development: {
-      host: "192.168.1.79",
-      port: 7545,
-      network_id: "*",
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
     },
+    // testnets
+    // properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    kovan: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/3c4247c6ee1a4f38aab4c59d7c90205a"),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
   },
-
-  // Add this field:
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
       version: "0.8.3",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
 }
